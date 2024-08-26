d138b5305c501cfaf2a85be989cb82aa4e1c66fe
Alex-Chis <i.alex.chis@gmail.com>
2024-08-26 21:39:49 +0300

    v1

---
+++ app/components/navbar.js
@@ -0,0 +1,20 @@
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Amx</h2>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
